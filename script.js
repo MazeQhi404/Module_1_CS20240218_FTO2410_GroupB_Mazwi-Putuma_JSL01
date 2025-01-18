9f6d78856a0ea26d9dc5299e008c911dae329bdc
MazeQhi404 <maze.putuma@gmail.com>
2025-01-19 01:51:50 +0200

    set result to valid if condition is met, invalid if it is not and set it to an empty string if there is no input.

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
+
+
 function validateSyntax() {
-    let input = document.getElementById('petInput').value;
+
+    input = document.getElementById('petInput').value;
+
     // Validation logic goes here
     let result = ''; // Placeholder for validation result
+    
 
     // TODO: Write your validation logic here
         // Check if input starts with 'pet_' and followed by alphanumeric characters
-if (input.substr(0,4) == "pet_" && !isNaN(input.substr(4,4))  ){
-    console.log("yes")
+if (input.substr(0,4) == "pet_" && !isNaN(input.substr(4,4)) && isNaN(input.substr(5)) && input.length > 8  ) {
+    
+    result = "Valid Syntax 🟢"
 
+} else if (input == "") { 
+    result = ''
+    
 } else {
-    console.log("no")
-
+    result = "Invalid Syntax 🔴"
 }
 
-            document.getElementById('result').innerText = result;
+    document.getElementById('result').innerText = result;
+    
 }
 
 validateSyntax()
 
+/* Questions to ask couch on Monday: 
+1. is there a way to access a span tag in js that does not contain text, just styling. incoporating whatever style you added in the stylesheet into your javascript file.
+*/
