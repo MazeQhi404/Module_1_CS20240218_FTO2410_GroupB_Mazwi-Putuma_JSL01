9cdc202de2793e2b6edbab3cb0bec65bda875590
MazeQhi404 <maze.putuma@gmail.com>
2025-01-18 21:17:17 +0200

    created a condition that validates whether the input starts with a string followed by numbers

--- script.js
+++ script.js
@@ -5,8 +5,16 @@ function validateSyntax() {
 
     // TODO: Write your validation logic here
         // Check if input starts with 'pet_' and followed by alphanumeric characters
+if (input.substr(0,4) == "pet_" && !isNaN(input.substr(4,4))  ){
+    console.log("yes")
+
+} else {
+    console.log("no")
+
+}
 
             document.getElementById('result').innerText = result;
 }
 
+validateSyntax()
 
